079590ab7bd273c6d883b0545d36a27262dd81b5
nathanaelchiang <chiang.na@northeatern.edu>
2024-02-23 23:29:20 -0500

    JavaScript and React.js Assignment 3

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,19 +1,25 @@
 import ModuleList from "../Modules/List";
+import Status from "./Status";
+import "./index.css";
 
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+      <div className="homeContainer">
+          <div className="moduleList">
+          <h2>Home</h2>
       <button type="button">Collapse All</button>
             <button type="button">View Progress</button>
             <select>
                 <option value="Publish All">Publish All</option>
             </select> 
-            <button type="button">Module</button>
-      <ModuleList />
-      <h2>Status</h2>
-    </div>
+            <button type="button" className="moduleButton">+Module</button>
+              <ModuleList />
+          </div>
+          <div className="status">
+              <Status />
+          </div>
+      </div>
   );
 }
 export default Home;
