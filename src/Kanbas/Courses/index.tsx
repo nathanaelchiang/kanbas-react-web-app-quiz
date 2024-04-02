f7f6430a7984f6b7ce16cb4dda09e94b0f0ee99c
nathanaelchiang <chiang.na@northeatern.edu>
2024-04-02 18:21:11 -0400

    a5 REST fa23

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -9,9 +9,12 @@ import Assignments from "./Assignments";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const API_BASE = process.env.REACT_APP_API_BASE;
+
 function Courses({ courses }: { courses: any[]; }) {
   const { courseId } = useParams();
-  const COURSES_API = "http://localhost:4000/api/courses";
+  /* const COURSES_API = "http://localhost:4000/api/courses"; */
+  const COURSES_API = `${API_BASE}/api/courses`;
   const [course, setCourse] = useState<any>({ _id: "" });
   const findCourseById = async (courseId?: string) => {
     const response = await axios.get(
